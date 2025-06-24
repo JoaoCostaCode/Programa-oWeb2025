dede3a811ee144c38bd9a84e4eb998d5a378e84c
Joao Costa <joao.costa.dev12@gmail.com.br>
2025-06-24 17:41:10 -0300

    Remover pasta do node incorreta

--- node/node2/utils/strings.js
+++
@@ -1,8 +0,0 @@
-const toUpper = (str) => str.toUpperCase()
-const toLower = (str) => str.toLowerCase()
-function createLink(filename) {
-    return `<a href="/${filename}">${filename}</a><br>\n`;
-    
-}
-
-module.exports = {toUpper, toLower, createLink}
